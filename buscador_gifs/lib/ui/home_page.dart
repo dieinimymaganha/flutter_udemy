bd9b8302e35da582b3ae35a046a31cdc4a37223c
Dieinimy <dieinimy@gmail.com>
2020-05-24 15:54:29 -0300

    Carregando mais gifs

--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
           'https://api.giphy.com/v1/gifs/trending?api_key=6wWPOLCBcUbhhqNxBgmeMz5HY5IlzEb2&limit=20&rating=G');
     } else {
       response = await http.get(
-          'https://api.giphy.com/v1/gifs/search?api_key=6wWPOLCBcUbhhqNxBgmeMz5HY5IlzEb2&q=$_search&limit=20&offset=$_offset&rating=G&lang=en');
+          'https://api.giphy.com/v1/gifs/search?api_key=6wWPOLCBcUbhhqNxBgmeMz5HY5IlzEb2&q=$_search&limit=19&offset=$_offset&rating=G&lang=en');
     }
     return json.decode(response.body);
   }
@@ -59,28 +59,35 @@ class _HomePageState extends State<HomePage> {
               ),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
             ),
           ),
           Expanded(
             child: FutureBuilder(
               future: _getGit(),
-              builder: (context, snapshot){
-                switch(snapshot.connectionState){
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
                   case ConnectionState.waiting:
                   case ConnectionState.none:
                     return Container(
                       width: 200.0,
                       height: 200.0,
-                      alignment:Alignment.center,
+                      alignment: Alignment.center,
                       child: CircularProgressIndicator(
                         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                         strokeWidth: 5.0,
                       ),
-
                     );
                   default:
-                    if(snapshot.hasError) return Container();
-                    else return _createGifTable(context, snapshot);
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      return _createGifTable(context, snapshot);
                 }
               },
             ),
@@ -94,17 +101,42 @@ class _HomePageState extends State<HomePage> {
     return GridView.builder(
         padding: EdgeInsets.all(10.0),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0
-        ),
-        itemCount: snapshot.data['data'].length,
-        itemBuilder:(context, index){
-          return GestureDetector(
-            child: Image.network(snapshot.data['data'][index]['images']['fixed_height']['url'], height: 300.0, fit: BoxFit.cover,),
-
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: _getCount(snapshot.data['data']),
+        itemBuilder: (context, index) {
+          if (_search == null || index < snapshot.data['data'].length) {
+            return GestureDetector(
+              child: Image.network(
+                snapshot.data['data'][index]['images']['fixed_height']['url'],
+                height: 300.0,
+                fit: BoxFit.cover,
+              ),
+            );
+          }
+          else return Container(
+            child: GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.add, color: Colors.white, size: 70.0,),
+                  Text('Carregar mais...', style: TextStyle(color: Colors.white, fontSize: 22.0),)
+                ],
+              ),
+              onTap: (){
+                setState(() {
+                  _offset += 19;
+                });
+              },
+            ),
           );
         });
+  }
 
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
   }
 }
